f3c5640a8379f648bb73050cc2a5bcfd6bc674ab
Jannick Berkhout <jannick@moodles.nl>
2018-05-30 15:26:09 +0200

    Add ability to use component as a plugin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+/**
+ * @author Moodles
+ * @author Jannick Berkhout (jannick@moodles.nl)
+ * @version 1.0.2
+ * @description A useful selectbox component for vue.js
+ */
+
 import SelectBox from './components/SelectBox.vue';
 
 export { SelectBox };
+
+window.VueSelectBox = {
+    install: function(Vue, options) {
+        Vue.component('SelectBox', SelectBox);
+    }
+}
